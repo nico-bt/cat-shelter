71fdb39422d14f53ff459b3e2fb9d08a36ad4eca
Nico Battaglia <nico-battaglia@hotmail.com>
2021-11-03 01:53:41 -0300

    Add image and admissionDate

--- models/cat.js
+++ models/cat.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const CatSchema = new mongoose.Schema({
   name: String,
   age: String,
+  image: {type: String, required: true},
   vaccinated: Boolean,
   neutered: Boolean,
-  adopted: Boolean
+  adopted: Boolean,
+  admissionDate: {type: Date, required: true, default: Date.now}
 });
 
 module.exports = mongoose.model("Cat", CatSchema)
